5f639e0a443a9c7dedc6b562d5c342a3d8aa5a2a
Delziron Braz <delziron.braz@hotmail.com>
2025-08-17 14:33:29 -0300

    initial commit

---
+++ app/quotes/api.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from app.quotes.core import get_quotes
+from app.quotes.schemas import SearchQuery, SearchResponse
+
+router = APIRouter()
+
+@router.post("/search/", response_model=SearchResponse)
+async def search(data: SearchQuery):
+    data = await get_quotes(data=data)
+    status, message = (True, "Success") if data else (False, "Thre are no results")
+
+    return SearchResponse(status=status, message=message, data=data)
